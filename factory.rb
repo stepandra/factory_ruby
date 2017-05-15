779e26c60e3d22ef17e15d8b4af24e8a620779c9
Stepan Mytchyk <st.mytchyk@gmail.com>
2017-05-16 02:10:56 +0300

    Catch exception and some refactor

--- factory.rb
+++ factory.rb
@@ -5,15 +5,20 @@ class Factory
       attr_accessor(*args)
 
       define_method :initialize do |*values|
-        @values = values
         args.each_with_index do |item, index|
+          raise NameError, "key #{item} isn't constant" unless item.is_a? Symbol
           public_send("#{item}=", values[index])
         end
       end
 
       def [](key)
-        key.is_a?(Numeric) ? @values[key] : public_send(key.to_sym)
+        if key.is_a?(Numeric)
+          instance_variable_get(instance_variables[key])
+        else
+          public_send(key.to_sym)
+        end
       end
+
       class_eval(&block) if block_given?
     end
   end
